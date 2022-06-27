dec11dbd15e40cc6124e9ea2cb24bb50a2f27eb3
Ozguc Dalga <ozgucdalga@gmail.com>
2022-06-27 19:40:40 +0000

    reading data

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -12,13 +12,29 @@ public class JDBCExecutor {
             "hplussport", "postgres", "password");
         try {
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+            CustomerDAO customerDao= new CustomerDAO(connection);
 
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1));
+          /**
+           * THIS IS FOR CREATING DATA FROM DATABASE
+           */
+          Customer customer = new Customer();
+            customer.setFirstName("Oguchi");
+            customer.setLastName("DALGA");
+            customer.setEmail("oguchi@yahoo.com");
+            customer.setPhone("555-444-3248");
+            customer.setAddress("185 Downsview Avenue");
+            customer.setCity("Toronto");
+            customer.setState("ON");
+            customer.setZipCode("M3M 1E7");
+            customerDao.create(customer);
 
-            }
+              /**
+              this is for READING DATA FROM DATABASE
+               */
+            Customer customer1= customerDao.findById(135);
+            System.out.println(customer1.getFirstName() + " " +customer1.getLastName());
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
