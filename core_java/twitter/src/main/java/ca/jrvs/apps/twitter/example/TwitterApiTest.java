66bd8d960cf805009ef83beaff56b1fafe3d965a
Ozguc Dalga <dalgaozguc@gmail.com>
2022-07-26 19:37:49 +0000

    httpjson

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
@@ -12,10 +12,10 @@ import java.util.Arrays;
 
 public class TwitterApiTest {
 
-  private static String CONSUMER_KEY= System.getenv ("consumerKey");
-  private static String CONSUMER_SECRET = System.getenv("consumerSecret");
-  private static String ACCESS_TOKEN =System.getenv("accessToken");
-  private static String TOKEN_SECRET = System.getenv("tokenSecret");
+  private static String CONSUMER_KEY= System.getenv ("Z2x4yT6EFlGq7ko46Kkig5ba0");
+  private static String CONSUMER_SECRET = System.getenv("N1K4BviusNHKjaw7vIyLV9Iex1bCV8ET4D55196nNiTqKZaa1z");
+  private static String ACCESS_TOKEN =System.getenv("1529479207817977856-3KW3Q3QcFcvBrcK7ijDEy8fZSOIcQR");
+  private static String TOKEN_SECRET = System.getenv("mkylqLYclnRJXksIlla8SyxKTlGCViW06iVTvby99ITLG");
 
 
   public static void main(String[] args)  throws Exception{
@@ -24,7 +24,7 @@ public class TwitterApiTest {
     OAuthConsumer consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
     consumer.setTokenWithSecret(ACCESS_TOKEN, TOKEN_SECRET);
 
-    //create an hhtp request
+    //create an http request
     String status  = "Today is a good day";
     PercentEscaper percentEscaper = new PercentEscaper(  "",  false);
     HttpPost request = new HttpPost(
@@ -40,21 +40,5 @@ public class TwitterApiTest {
       HttpClient httpClient = HttpClientBuilder.create().build();
       HttpResponse response = httpClient.execute(request);
       System.out.println(EntityUtils.toString(response.getEntity()));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }
-
 }
