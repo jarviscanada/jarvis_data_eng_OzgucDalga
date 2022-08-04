41721fe4b642c4ca51eefd4a87022c541c142921
Ozguc Dalga <dalgaozguc@gmail.com>
2022-08-04 01:00:09 +0000

    Dao ticket is implemented

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Entities.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Entities.java
@@ -1,2 +1,27 @@
-package ca.jrvs.apps.twitter.model;public class Entities {
+package ca.jrvs.apps.twitter.model;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class Entities {
+
+
+        private Hashtag[] hashtags;
+        private UserMention[] user_mentions;
+
+        public Hashtag[] getHashtags() {
+            return hashtags;
+        }
+
+        public void setHashtags(Hashtag[] hashtags) {
+            this.hashtags = hashtags;
+        }
+
+        public UserMention[] getUser_mentions() {
+            return user_mentions;
+        }
+
+        public void setUser_mentions(UserMention[] user_mentions) {
+            this.user_mentions = user_mentions;
+        }
 }
